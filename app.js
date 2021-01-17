1f96d82aecfc6d8636516c38c81686b52f83de59
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-01-17 14:16:00 +0530

    feature taking input by calender

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ var userInput=document.querySelector("#txt-input")
 var but=document.querySelector("#btn")
 var output=document.querySelector("#output")
 
+function getDate(put){
+    var date= put.split("-")
+   // console.log(date[0])
+    return date[0]
+
+}
+
 function isLeapYear(input){
     var leap=true
+    console.log(input)
     if(input%4===0){
         if(input%100===0){
             if(input%400===0){
@@ -25,7 +33,13 @@ function isLeapYear(input){
 
 function clickFunction(){
     var num=userInput.value;
-    var toCheck=isLeapYear(num);
+    /*var year=num.split("-")
+    var numb=year[0]
+    console.log(numb)
+    var toCheck=isLeapYear(numb);
+another way of doing it without declaring getdate function*/
+
+    var toCheck=isLeapYear(getDate(num));
 
     if(toCheck===true){
         output.innerText="You were Born On The Leap Year"
